5b37dd95e0249642888cd70857c48eb28320e1f3
ZHtmn <a_zhelobtsov@bk.ru>
2022-11-29 16:26:28 +0500

    dbcreate

---
+++ db/migrations/20221129084919-create-drug.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Drugs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      logo: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      sale: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      count: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Drugs');
+  }
+};
